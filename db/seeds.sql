098aa25c694b51770b68720722fa10e106b9d4f0
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-06-27 19:09:39 -0400

    working app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('cheeseburger');
+INSERT INTO burgers (name) VALUES ('double cheesburger');
+INSERT INTO burgers (name, eaten) VALUES ('hamburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('bacon cheese burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('1/4 pounder', true);
+INSERT INTO burgers (name) VALUES ('vegiburger');
